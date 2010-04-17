845ec6ee22b06bb8b1256c314e3e10f2ede19c4f
Stefan Maier <stefanmaier@gmail.com>
2010-04-17 17:23:08 +0200

    And require yaml

--- lib/warcorrespondent.rb
+++ lib/warcorrespondent.rb
@@ -1,6 +1,7 @@
 require 'warcorrespondent/reporter.rb'
 require 'warcorrespondent/uplink.rb'
 require 'json'
+require 'yaml'
 
 module WarCorrespondent
   CONFIG_FILE = '/etc/warcorrespondent/warcorrespondent.yml'
