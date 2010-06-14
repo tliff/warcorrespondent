a96c4b50d1c21aafd08311f23dae08379446c702
Stefan Maier <stefanmaier@gmail.com>
2010-06-14 12:31:01 +0200

    fix bugs in the installer

--- lib/warcorrespondent.rb
+++ lib/warcorrespondent.rb
@@ -8,15 +8,18 @@ module WarCorrespondent
   CONFIG_FILE = '/etc/warcorrespondent/warcorrespondent.yml'
   REPORTERS_DIRECTORIES = '/etc/warcorrespondent/reporters'
 
-  def config_file
-    [ 'warcorrespondent.yml'
-      '/etc/warcorrespondent/warcorrespondent.yml',
-      '~/.warcorrespondent/warcorrespondent.yml'].each do |f|
-        return f if File.exists?(f)
+  def self.config_file
+    [
+      "warcorrespondent.yml",
+      "/etc/warcorrespondent/warcorrespondent.yml",
+      '~/.warcorrespondent/warcorrespondent.yml'
+    ].each do |f|
+        return File.expand_path(f) if File.exists?(File.expand_path(f))
       end
+    false
   end
-  
-  def install
+
+  def self.install
     puts <<-EOT
     warcorrespondent could not find your config file and will now guide you
     through the setup process.
@@ -24,51 +27,59 @@ module WarCorrespondent
     Do you want to install system-wide (1) or in your home directory(2)? [1]
     EOT
     basepath = nil
-    while cmd = gets.strip && ['1', '2', ''].member?(cmd) do end
-    if cmd == '1' || cmd == '' do
+    while (cmd = gets.strip) && !['1', '2', ''].member?(cmd) do end
+    if cmd == '1' || cmd == '' 
       basepath = '/etc/warcorrespondent'
     elsif cmd == '2'
-      basepath = '~/.warcorrespondent'
+      basepath = File.expand_path('~/.warcorrespondent')
     end
     #create the directory if necessary
     if !File.exists?(basepath) then Dir.mkdir(basepath) end
     if !File.exists?(basepath+"/reporters") then Dir.mkdir(basepath+"/reporters") end
     config = {}
     puts <<-EOT
-      Where do you want warcorrespondent to report to?
-      If you want to report to warroom, just press enter [http://warroom.tliff.de/reports]
+
+
+Where do you want warcorrespondent to report to?
+If you want to report to warroom, just press enter [http://warroom.tliff.de/reports]
     EOT
     url = gets.strip
     config[:url] = url.empty? ? "http://warroom.tliff.de/reports" : url
     puts <<-EOT
-      What is the secret this warcorrespondent instance will
-      use to authenticate to warroom?
+
+
+What is the secret this warcorrespondent instance will
+use to authenticate to warroom?
     EOT
-    while config[:secret] = gets.strip && config[:secret].empty? do end
+    while (config[:secret] = gets.strip) && config[:secret].empty? do end
     File.open( "#{basepath}/warcorrespondent.yml", 'w+' ) do |f|
       YAML.dump(config, f)
     end
     
     puts <<-EOT
-    warcorrespondent can now set up basic reporting for this host.
-    Would you like to have that set up for you(y/n)[y]?
+
+
+warcorrespondent can now set up basic reporting for this host.
+Would you like to have that set up for you(y/n)[y]?
     EOT
-    while cmd = gets.strip && ['y', 'n', ''].member?(cmd) do end
-    if cmd == 'y' || cmd == '' do
-      hostname = `hostname`
+    while (cmd = gets.strip) && !['y', 'n', ''].member?(cmd) do end
+    if cmd == 'y' || cmd == ''
+      hostname = `hostname`.strip
       basic = <<-EOT
       WarCorrespondent::Reporters::CPU.new(:identifier => 'hosts:#{hostname}:stats')
       WarCorrespondent::Reporters::LoadAvg.new(:identifier => 'hosts:#{hostname}:stats')
       WarCorrespondent::Reporters::Memory.new(:identifier => 'hosts:#{hostname}:stats')
       WarCorrespondent::Reporters::Net.new(:identifier => 'hosts:#{hostname}:stats')
       EOT
-      File.new("#{basepath}/reporters/basic.rb", "w+") do |f|
+      File.open("#{basepath}/reporters/basic.rb", "w+") do |f|
         f.write(basic)
       end
     end
     
     puts <<-EOT
-    Setup is now complete.
+
+
+Setup is now complete.
     EOT
     
   end
